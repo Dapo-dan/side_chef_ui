6d3a7a54a95b85087f7e0dd0462f7e6af15926e0
Dapo <odapo138@gmail.com>
2022-02-05 22:43:39 +0100

    sign up screen

--- lib/accountSetup/signUP.dart
+++ lib/accountSetup/signUP.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:side_chef_ui/accountSetup/choiceScreen.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -6,26 +7,155 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Icon(Icons.arrow_back),
-          const Text(
-            'Sign Up',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const Text(
-            'Save delicious recipes and get personalized',
-            style: TextStyle(fontSize: 10),
-          ),
-          const Text(
-            'content',
-            style: TextStyle(fontSize: 10),
-          ),
-          Column(
-            children: [TextFormField()],
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20.0, 60, 20, 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Icon(Icons.arrow_back),
+            const SizedBox(
+              height: 45,
+            ),
+            const Text(
+              'Sign Up',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'Save delicious recipes and get personalized',
+              style: TextStyle(fontSize: 20),
+            ),
+            const Text(
+              'content',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Display Name',
+                  ),
+                  onSaved: (String? value) {
+                    // This optional block of code can be used to run
+                    // code when the user saves the form.
+                  },
+                  validator: (String? value) {
+                    return (value != null && value.contains('@'))
+                        ? 'Do not use the @ char.'
+                        : null;
+                  },
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                  onSaved: (String? value) {
+                    // This optional block of code can be used to run
+                    // code when the user saves the form.
+                  },
+                  validator: (String? value) {
+                    return (value != null && value.contains('@'))
+                        ? 'Do not use the @ char.'
+                        : null;
+                  },
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  onSaved: (String? value) {
+                    // This optional block of code can be used to run
+                    // code when the user saves the form.
+                  },
+                  validator: (String? value) {
+                    return (value != null && value.contains('@'))
+                        ? 'Do not use the @ char.'
+                        : null;
+                  },
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Confirm Password',
+                  ),
+                  onSaved: (String? value) {
+                    // This optional block of code can be used to run
+                    // code when the user saves the form.
+                  },
+                  validator: (String? value) {
+                    return (value != null && value.contains('@'))
+                        ? 'Do not use the @ char.'
+                        : null;
+                  },
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const Icon(Icons.ac_unit),
+                const SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'I would like to receive recipe inspiration, meal',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      'plans, updates, and more!',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                    minimumSize: const Size(400, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    backgroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ChoiceScreen()),
+                    );
+                  },
+                  child:
+                      const Text('NEXT', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 100,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:side_chef_ui/accountSetup/signUP.dart';
 import 'package:side_chef_ui/intro/splashScreen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
+      home: const SignUp(),
     );
   }
 }
