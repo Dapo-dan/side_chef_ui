3de48e391d699f66c80181da84ec67f9d08ceaa5
Dapo <odapo138@gmail.com>
2022-02-07 23:39:22 +0100

    home page

--- lib/home/homePage.dart
+++ lib/home/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:side_chef_ui/accountSetup/purposeScreen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,47 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(10, 70, 10, 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Icon(
+                  Icons.account_circle,
+                  size: 40,
+                ),
+                const Spacer(),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    backgroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const PurposeScreen()),
+                    );
+                  },
+                  child: Icon(Icons.card_travel),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            const Text('Daily Inspiration',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50)),
+            const SizedBox(
+              height: 20,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
